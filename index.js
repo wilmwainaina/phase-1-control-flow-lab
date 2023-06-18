0ec26c98a944e6458feab8b4817dfda95a59f470
wilmwainaina <wilfred.wainaina@student.moringaschool.com>
2023-06-18 17:55:15 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride<=400){
+    return'This one is on me!'
+  }
+  else if(ride>=2001){
+    return'I will gladly take your thirty bucks.'
+  }
+  else if(ride>=2501){
+    return 'No can do.'
+  }
+  
+  }
+
+
+
+function ternaryCheckCity(area){
+ return area ==='NYC'?"Ok, sounds good." :'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+  case 'generous':
+    return "Thank you so much."
+  case 'not as generous':
+    return"Thank you."
+  case 'thanks for everything':
+    return "Bye."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
